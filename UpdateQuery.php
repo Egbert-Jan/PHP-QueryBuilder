e1b7944faee80a2c668b42831bd205ad6d749743
Egbert-Jan <terpstra@d-tt.nl>
2020-11-20 17:49:22 +0100

    update saver and fixed typo

--- UpdateQuery.php
+++ UpdateQuery.php
@@ -2,41 +2,37 @@
 
 
 class UpdateQuery extends WhereQuery {
-
-    //UPDATE Customers
-    //SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
-    //WHERE CustomerID = 1;
-
+    
     private $keyValues = [];
 
     public function set($keyVals) {
-        $this->keyValues = $keyVals;
+        foreach($keyVals as $key => $val) {
+            array_push($this->keyValues, new KeyValClause($key, "=", $val));
+        }
+
         return $this;
     }
 
     public function exec() {
         $sql = "UPDATE " . $this->table . " SET ";
 
-        $keys = array_keys($this->keyValues);
-        foreach($keys as &$key) {
-            $sql .= $key . " = :".$key . ",";
+        $keyVals = $this->keyValues;
+        foreach ($keyVals as $clause) {
+            $sql .= $clause->key . "=" . $clause->placeholder . ",";
         }
-        unset($val);
         $sql = rtrim($sql, ','); //Remove last comma
 
         $sql = $this->createWhere($sql);
-
         $prepared = $this->pdo->prepare($sql);
 
-        //NIET GOED ALS 2 DE ZELFDE PARAMETERS ZIJN
-        foreach ($keys as $key) {
-            $prepared->bindValue(":".$key, $this->keyValues[$key]);
+        foreach ($this->keyValues as $clause) {
+            $prepared->bindValue($clause->placeholder, $clause->value);
         }
 
         foreach ($this->where as $clause) {
             $prepared->bindValue($clause->placeholder, $clause->value);
         }
-        // print_r($prepared);
+        print_r($prepared);
         return $prepared->execute();
     }
 
